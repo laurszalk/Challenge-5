5f381034e9d12c1749c774bf79ac10323b066384
laurszalk <laurenszalk@gmail.com>
2023-01-06 14:46:57 -0500

    updated time slots in html and js time display

--- Develop/script.js
+++ Develop/script.js
@@ -23,8 +23,12 @@
 //
 // });
 
+//displays current day and time at the top of page
 var timeDisplayEl = $("#currentDay");
 function displayTime() {
-  var rightNow = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
+  var rightNow = dayjs().format("dddd, MMM DD, YYYY [at] hh:mm:ss a");
   timeDisplayEl.text(rightNow);
 }
+
+displayTime();
+setInterval(displayTime, 1000);
