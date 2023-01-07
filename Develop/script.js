991cb4a0f6e91529c5c7db1dfc855b62dcdf2afa
laurszalk <laurenszalk@gmail.com>
2023-01-06 19:12:36 -0500

    handle click event function

--- Develop/script.js
+++ Develop/script.js
@@ -23,12 +23,59 @@
 //
 // });
 
+// textarea elements
+var time9am = $("#9am");
+var time10am = $("#10am");
+var time11am = $("#11am");
+var time12pm = $("#12pm");
+var time1pm = $("#1pm");
+var time2pm = $("#2pm");
+var time3pm = $("#3pm");
+var time4pm = $("#4pm");
+var time5pm = $("#5pm");
+
+var timeElArray = [
+  time9am,
+  time10am,
+  time11am,
+  time12pm,
+  time1pm,
+  time2pm,
+  time3pm,
+  time4pm,
+  time5pm,
+];
+
 //displays current day and time at the top of page
 var timeDisplayEl = $("#currentDay");
 function displayTime() {
   var rightNow = dayjs().format("dddd, MMM DD, YYYY [at] hh:mm:ss a");
   timeDisplayEl.text(rightNow);
 }
+
+// function for handling clicks
+var saveBttn = $(".saveBtn");
+function submitForm(event) {
+  event.preventDefault();
+
+  var clickBtn = $(event.currentTarget);
+  var text = clickBtn.siblings("textarea");
+  var time = text.data("hour");
+
+  localStorage.setItem("time" + time, text.val());
+}
+
+saveBttn.on("click", submitForm);
+
+// function submitFrom(event) {
+//   event.preventDefault();
+
+//   var clickBtn = $(event.currentTarget);
+//   var text = clickBtn.siblings("textarea");
+//   var time = text.data("hour");
+
+//   localStorage.setItem("time" + time, text.val());
+// }
 
 displayTime();
 setInterval(displayTime, 1000);
